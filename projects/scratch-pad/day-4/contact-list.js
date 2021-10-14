acb4f9e9029c9d660135ef7e6a598159819b91b1
John L. Dyer <jldyer85@gmail.com>
2021-10-14 20:41:42 +0000

    W1D4 Contacts List

--- projects/scratch-pad/day-4/contact-list.js
+++ projects/scratch-pad/day-4/contact-list.js
@@ -21,7 +21,8 @@
  *         undefined if the fullName does not match any contacts in the list.
  *      4. removeContact(contact): takes a contact object to be removed from 
  *         the contact-list.
- *      5. add a printAllContactNames() Function to your makeContactList() factory. The printAllContactNames() Function should 
+ *      5. add a printAllContactNames() Function to your makeContactList() factory. 
+ *          The printAllContactNames() Function should 
  *         return a String formated with all the full-names of the separated 
  *         with a line-break, like so:
  *          
@@ -35,7 +36,11 @@
 
 // YOUR CODE GOES BELOW HERE //
 function makeContact(id, nameFirst, nameLast) {
-
+    return {
+        id: id,
+        nameFirst: nameFirst,
+        nameLast: nameLast,
+    }
 } 
 
 
@@ -43,13 +48,58 @@ function makeContactList() {
     /*
      * You need something here to hold contacts. See length api for a hint:
      */
-    var contacts;
+    var contacts = [];
     
     return {
         // we implemented the length api for you //
         length: function() {
             return contacts.length;
+        },
+        //addContact key with function that takes a contact arg and pushes it into contacts arr
+        addContact: function(contact){
+            return contacts.push(contact)
+        },
+        //findContact key with function that takes a fullName 
+        findContact: function(fullName){
+            //loop through contacts array to get access to contacts
+            for(var i = 0; i < contacts.length; i++){
+
+                //interpolate contact obj first & last name to compare to fullName
+                if(`${contacts[i].nameFirst} ${contacts[i].nameLast}` === fullName){
+
+                    //if equal, return contact obj
+                    return contacts[i];
+                }
+            }
+        },
+        removeContact: function(contact){
+            return contacts.pop();
+        },
+        //printAllNames from contacts list in string with new line for each name
+        printAllContactNames: function(){
+            //create var to store final list of names
+            var returnList = ""
+
+            //loop through contacts list obj to get access to names
+            for(var i = 0; i < contacts.length; i++){
+
+                //create var to store full name from contact obj
+                var fullName = `${contacts[i].nameFirst} ${contacts[i].nameLast}`;
+
+              //if last contact reached return full name with no new line break  
+              if(i === contacts.length - 1){
+                  returnList += `${fullName}`;
+
+              //else return fullName with new line break  
+              } else {
+                  returnList += `${fullName}\n`
+              }
+            }
+
+            //return final list of names
+            return returnList;
         }
+
     }
 }
 
